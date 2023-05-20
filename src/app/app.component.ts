076f420672e12b0e75c64de4098435b4429cac51
Philipp Leonov <philippleonov@mail.ru>
2023-05-20 14:31:00 +0600

    #books - Приложените создано

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'books';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    const a = {
+      a: 1,
+    };
+    console.log(a + '')
+  }
 }
